79a92f17e5690fac90ed20a2af0f378f50907141
KKW557 <kevin557557@gmail.com>
2026-03-27 17:24:22 +0800

    gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,34 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    alias(libs.plugins.kotlin)
+}
+
+repositories {
+    gradlePluginPortal()
+}
+
 val minecraft = libs.versions.minecraft.get()
+
+val targetJavaVersion = 25
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions.jvmTarget.set(JvmTarget.fromTarget(targetJavaVersion.toString()))
+}
 
 subprojects {
     project.version = "${project.version}+${minecraft}"
 
     plugins.withId("java") {
         extensions.configure<JavaPluginExtension> {
-            toolchain.languageVersion = JavaLanguageVersion.of(25)
+            toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
             withSourcesJar()
+        }
+
+        tasks.withType<JavaCompile>().configureEach {
+            options.encoding = "UTF-8"
+            options.release.set(targetJavaVersion)
         }
 
         tasks.withType<Jar>().configureEach {
